6f4bafa13e524dc0557a66b613ba5c06d1d81eba
Samuel Ilelakinwa <ilelakinwa.samuel@gmail.com>
2023-08-08 11:00:19 +0100

    updated commit

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const imageDownloader = require("image-downloader");
 const multer = require("multer");
 const fs = require("fs");
 const mime = require("mime-types");
+const Place = require("./models/Place.js");
+
+// Middleware
+app.use(express.json());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
+// Upload by Link
 app.post("/api/upload-by-link", async (req, res) => {
   const { link } = req.body;
   const newName = "photo" + Date.now() + ".jpg";
@@ -19,7 +28,10 @@ app.post("/api/upload-by-link", async (req, res) => {
   res.json(url);
 });
 
+// Multer Middleware for Photo Upload
 const photosMiddleware = multer({ dest: "/tmp" });
+
+// Upload Photos
 app.post(
   "/api/upload",
   photosMiddleware.array("photos", 100),
@@ -33,3 +45,100 @@ app.post(
     res.json(uploadedFiles);
   }
 );
+
+// Create a Place
+app.post("/api/places", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { token } = req.cookies;
+  const {
+    title,
+    address,
+    addedPhotos,
+    description,
+    price,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.create({
+      owner: userData.id,
+      price,
+      title,
+      address,
+      photos: addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+    });
+    res.json(placeDoc);
+  });
+});
+
+// Get User's Places
+app.get("/api/user-places", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await Place.find({ owner: id }));
+  });
+});
+
+// Get Place by ID
+app.get("/api/places/:id", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { id } = req.params;
+  res.json(await Place.findById(id));
+});
+
+// Update a Place
+app.put("/api/places", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { token } = req.cookies;
+  const {
+    id,
+    title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.findById(id);
+    if (userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
+        title,
+        address,
+        photos: addedPhotos,
+        description,
+        perks,
+        extraInfo,
+        checkIn,
+        checkOut,
+        maxGuests,
+        price,
+      });
+      await placeDoc.save();
+      res.json("ok");
+    }
+  });
+});
+
+// Get All Places
+app.get("/api/places", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  res.json(await Place.find());
+});
--- api/models/Place.js
+++ api/models/Place.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
+// Define the schema for a Place
 const placeSchema = new mongoose.Schema({
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Reference to the User model
   title: String,
   address: String,
-  photos: [String],
+  photos: [String], // Array of photo URLs
   description: String,
-  perks: [String],
+  perks: [String], // Array of perks or amenities
   extraInfo: String,
+  checkIn: Number,
+  checkOut: Number,
   maxGuests: Number,
   price: Number,
 });
 
+// Create a model based on the schema
 const PlaceModel = mongoose.model("Place", placeSchema);
 
+// Export the model to be used in other parts of the application
 module.exports = PlaceModel;
